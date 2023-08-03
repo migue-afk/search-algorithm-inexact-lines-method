efc2d63a5b1bdcdfd3c2541abed9ee15b356d49d
migueafk <miguef10hd@gmail.com>
2023-08-03 18:16:16 -0500

    adding comments

--- rein.m
+++ rein.m
@@ -1,6 +1,6 @@
 function Result=rein(R,Li)
 %Function describing the search for the minimum of the equation R.
-%Li is the lower limit.
+%Li is the lower limit of search.
 
 
 x0=Li;                      %Parameters of the fuction
@@ -9,12 +9,18 @@ beta=0.1;
 alfa=0.9;
 d=1
 
-f=inline(R);                
-dxf1=diff(sym(R))           
-dx=inline(dxf1)
+f=inline(R);                %Create a function in line, from a user created
+                            %expression in this case the fuction is R
+dxf1=diff(sym(R))           %First derivative of R
+dx=inline(dxf1)             %Create a function in line, from a user created
+                            %expression in this case, first derivative of R dxf1
 
 
-for i = 1:70
+for i = 1:70                                %70 is the number of iterations to search
+                                            %for the minimum, this number can be added %or subtracted, it all depends od the %precision of the method
+
+%Algorithm of the search inexact lines
+
     A=f(x0+(delt*d))
     B=f(x0)+(beta*delt*dx(x0)*d)
 
